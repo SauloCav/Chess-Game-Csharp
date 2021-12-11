253d4ce035df273904c77228ed8eb2a807a89b88
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-12-11 18:58:37 -0300

    update project

---
+++ xadrez-console/tabuleiro/Cor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace xadrez_console.tabuleiro
+{
+    enum Cor
+    {
+
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+
+    }
+}
