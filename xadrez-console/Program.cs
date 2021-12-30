f422cc804d1f92c76f2bc459463072f7578c38d0
Saulo Cavalcante <saulocavalcante25@hotmail.com>
2021-12-30 17:58:13 -0300

    update project

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,8 +12,19 @@ namespace xadrez_console
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
 
-                Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.WriteLine("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
 
             }
             catch (TabuleiroException e)
